01bcb7177502e3cd1fe787d2c93f8ca04f809bb5
EYoung75 <eyoung75.ey@gmail.com>
2020-01-21 10:47:45 -0600

    refined like post looping conditions

--- automation.py
+++ automation.py
@@ -6,63 +6,63 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-class Bot:
-    driver = webdriver.Chrome("./chromedriver")
+# class Bot:
+#     driver = webdriver.Chrome("./chromedriver")
 
-    def __init__(self, username, password):
-        super().__init__()
-        self.username = username
-        self.password = password
-        self.login()
+#     def __init__(self, username, password):
+#         super().__init__()
+#         self.username = username
+#         self.password = password
+#         self.login()
 
-    def login(self):
-        if(self.username != ""):
-            self.driver.get(
-                "https://www.instagram.com/accounts/login/?source=auth_switcher"
-            )
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(self.username)
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys(self.password)
-            self.driver.find_elements_by_tag_name("button")[1].click()
-            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "HoLwm"))).click()
+#     def login(self):
+#         if(self.username != ""):
+#             self.driver.get(
+#                 "https://www.instagram.com/accounts/login/?source=auth_switcher"
+#             )
+#             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys(self.username)
+#             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys(self.password)
+#             self.driver.find_elements_by_tag_name("button")[1].click()
+#             WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "HoLwm"))).click()
         
 
 
-bot_one = Bot(input("username: "), input("password: "))
+# bot_one = Bot(input("username: "), input("password: "))
 
 
 # ///// LOGIN SEQUENCE
-# driver = webdriver.Chrome("./chromedriver")
+driver = webdriver.Chrome("./chromedriver")
 
-# driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+driver.get("https://www.instagram.com/accounts/login/?source=auth_switcher")
+driver.maximize_window()
 
-# username = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.NAME, "username"))
-# ).send_keys("USERNAME")
-# password = WebDriverWait(driver, 10).until(
-#     EC.presence_of_element_located((By.NAME, "password"))
-# ).send_keys("PASSWORD")
+username = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "username"))).send_keys("")
+password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password"))).send_keys("")
 
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "button")))
-# print(driver.find_elements_by_tag_name("button")[1].click())
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "button")))
+print(driver.find_elements_by_tag_name("button")[1].click())
 
-# WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "HoLwm"))).click()
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "HoLwm"))).click()
 # ////
 
 # //// LIKE POST SEQUENCE
-# last_post = False
+finished = 0
 
-# while(last_post == False):
-#     doc_body = driver.find_element_by_tag_name("body")
-#     doc_body.send_keys(Keys.PAGE_DOWN)
-#     likes = driver.find_elements_by_xpath("//button/*[name()='svg'][@aria-label='Like']")
-#     for like in likes:
-#         like.click()
-#         time.sleep(.2)
-#         print("like")
-#     try:
-#         driver.find_element_by_xpath("//button/*[name()='svg'][@aria-label='Unlike']")
-#         print("breaking")
-#         break
-#     except:
-#         continue
+# while(EC.visibility_of_any_elements_located(driver.find_elements_by_xpath("//button/*[name()='svg'][@aria-label='Like']"))):
+while finished < 8:
+    doc_body = driver.find_element_by_tag_name("body")
+    doc_body.send_keys(Keys.PAGE_DOWN)
+    likes = driver.find_elements_by_xpath("//button/*[name()='svg'][@aria-label='Like']")
+    for like in likes:
+        like.click()
+        time.sleep(.02)
+        print("like")
+    try:
+        # EC.invisibility_of_element_located(driver.find_elements_by_xpath("//button/*[name()='svg'][@aria-label='Like']"))
+        driver.find_element_by_xpath("//button/*[name()='svg'][@aria-label='Unlike']")
+        print("breaking")
+        finished += 1
+        # last_post = True
+    except:
+        continue
 # //////
